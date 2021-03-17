d19fc6c8b33b42850b465692ab6c12935099d871
xXKalinaXx <73699117+xXKalinaXx@users.noreply.github.com>
2021-03-17 11:24:20 +0100

    Made Wall GameObject

--- Wall.cs
+++ Wall.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PacMan
 {
-    class Wall
+    class Wall : GameObject
     {
+        public override void LoadContent(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
